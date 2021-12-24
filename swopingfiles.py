5bfb40858d476ed535132ee2d749288efe792fb9
nanwabA <75519223+nanwabA@users.noreply.github.com>
2021-12-24 19:41:26 +0530

    Add files via upload

---
+++ swopingfiles.py
@@ -0,0 +1,17 @@
+def swappingData():
+    file1 = input("enter your file 1:")
+    file2 = input("enter your file 2:")
+
+    with open(file1,"r")as a:
+        data_a=a.read()
+        with open(file2,"r")as b:
+            data_b=b.read()
+    with open(file1,"w")as a:
+        a.write(data_b)
+    with open(file2,"w")as b:
+        b.write(data_a)
+
+swappingData()           
+
+                
+
